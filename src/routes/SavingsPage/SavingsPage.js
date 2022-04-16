5128341046fda7324734efc0af11e979f2aa3cee
Lindsey Zylstra <lindseyzylstra@gmail.com>
2022-04-15 17:07:06 -0700

    Adds initial displaying of savings list

--- src/routes/SavingsPage/SavingsPage.js
+++ src/routes/SavingsPage/SavingsPage.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
+import SavingsRow from "../../components/SavingsRow/SavingsRow"
 import "./SavingsPage.css";
 
 
 class SavingsPage extends Component {
+  renderSavingsList(allsavings){
+    // Has all savings from all budgets so need to map through all of them
+   
+    return allsavings.map((savings, ind) => (
+
+      savings.map((savingitem, index) => (
+      //  console.log(savingitem)
+      <SavingsRow
+        id={savingitem.savings_id}
+        name={savingitem.savings_name}
+        amount={savingitem.savings_amount}
+        goal_date={savingitem.goal_date}
+        goal={savingitem.goal_amount}
+        monthly_auto={savingitem.monthly_auto}
+        key={index}
+      />
+       ))
+      )); 
+     
+// return test;
+  }
+  
   render() {
+    console.log(this.props.savingsList)
+    let list = this.props.savingsList;
     return (
       <div className="savings_page">
-        <p className="app_description">
-         this will be the savings page
-        </p>
+        <h2 className="page_title">Savings page</h2>
+
+        {this.renderSavingsList(list)}
+      
         
 
       </div>
